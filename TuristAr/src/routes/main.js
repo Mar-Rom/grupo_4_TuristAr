88805c366269f3e7a460722ec02a92123f61a626
Jose-Carabajal16 <jose.carabajal16@gmail.com>
2023-10-25 18:23:53 -0300

    se modifico amin.js

--- TuristAr/src/routes/main.js
+++ TuristAr/src/routes/main.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const mainControlles = require('../controllers/mainControllers');
+/**Express-validator */
+const { validacion, result} = require('../middleware/validationLogin')
 
 //  ↓↓↓ router.get('/', MainController) ↓↓↓
 router.get('/', mainControlles.home);
 router.post('/', mainControlles.home);
 
 router.get('/login', mainControlles.login)
+router.post('/login', validacion, result, mainControlles.processLogin)
 
 
 router.get('/register', mainControlles.register)
