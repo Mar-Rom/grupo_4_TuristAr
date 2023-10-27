c518b6849868ad48fb2937e211db8186373b814a
rhovelazquez <rocyvelazquez2021@gmail.com>
2023-10-27 10:47:27 -0300

    implementacion de registro de usuario junto con sus validaciones, y el hashin, se agrego otro multer para que guarde las imagenes de user en la carpeta user

--- TuristAr/src/routes/main.js
+++ TuristAr/src/routes/main.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const mainControlles = require('../controllers/mainControllers');
-
+const {arrayValidaciones, validateCreateForm}=require("../middleware/validationRegister");
+const upload = require('../middleware/multerUser');
 //  ↓↓↓ router.get('/', MainController) ↓↓↓
 router.get('/', mainControlles.home);
-router.post('/', mainControlles.home);
+
 
 router.get('/login', mainControlles.login)
 
 
 router.get('/register', mainControlles.register)
-router.post('/register', mainControlles.register)
+router.post('/register',upload.single('image'),arrayValidaciones,validateCreateForm, mainControlles.createUser)
 
 router.get('/valid', mainControlles.valid)  
 
