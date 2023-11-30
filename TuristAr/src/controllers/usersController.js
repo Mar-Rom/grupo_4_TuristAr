6def5cf97b7045674353a4c508383917d2a5d4a1
rhovelazquez <rocyvelazquez2021@gmail.com>
2023-11-30 16:59:46 -0300

    al editar la contraseña del perfil, se la vuelve a hashear, se agrego el multer en el caso de querer cambiar imagen del user

--- TuristAr/src/controllers/usersController.js
+++ TuristAr/src/controllers/usersController.js
@@ -2,6 +2,7 @@ const dataUsers = require('../data/users.json');
 const fs = require('fs');
 const path = require('path');
 const userFilePath = path.join(__dirname, '..', 'data', 'users.json');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
     profile: (req, res) => {
@@ -21,10 +22,23 @@ module.exports = {
         let usuariIndex = dataUsers.findIndex((user)=> user.id == id);
         let usuarioAEditar = dataUsers[usuariIndex];
         //volvemos a hashear en caso de que la contraseña haya sido cambiada
-
+        let band=false; //variables auxiliares
+        let hashPassword ="" 
         let usuarioEditado = req.body;
         usuarioEditado.id = parseInt(id);
         usuarioEditado.image = req.file?.filename || usuarioAEditar.image;
+        // verifico si hay algun cambio en el campo de la constraseña 
+        if (usuarioAEditar.password2 != req.body.password)
+        {
+            hashPassword = bcrypt.hashSync(req.body.password, 10);
+            band=true;
+            usuarioEditado.password2=req.body.password;
+        }
+        //si la bandera band cambia a true la contraseña se actualizara si no seguira igual
+        usuarioEditado.password2=  band?req.body.password: usuarioAEditar.password2
+        usuarioEditado.password=  band?hashPassword: usuarioAEditar.password;
+        //el password2 debe ir primero de otro modo password y password2 tendran el valor de la contraseña hasheda
+
 
         for(let propiedad in usuarioAEditar){
             if(usuarioAEditar.hasOwnProperty(propiedad)){
